894f49d606b234e1e52bcb793f1f1b658241e3ec
vinnivso <vinnivsob@gmail.com>
2022-01-19 21:31:00 -0300

    Feat: Erro01 - sintaxe, semanática e execução

---
+++ 00-comecando-com-java/3-entendendo-erros/Erro01-Solved.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
+// Lembrando que quando compilar, será gerado um arquivo compilado com o nome da respectiva classe no código.
