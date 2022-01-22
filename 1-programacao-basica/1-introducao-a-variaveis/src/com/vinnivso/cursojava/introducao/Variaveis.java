3fdefc9737198d5b3e38b1260eff5a20e0ce1e8c
vinnivso <vinnivsob@gmail.com>
2022-01-22 13:42:30 -0300

    Feat: introducao-a-variaveis

---
+++ 1-programacao-basica/1-introducao-a-variaveis/src/com/vinnivso/cursojava/introducao/Variaveis.java
@@ -0,0 +1,19 @@
+package com.vinnivso.cursojava.introducao;
+
+public class Variaveis {
+    public static void main(String[] args) {
+
+        //Declarando a variável
+        int idade = 27;
+        String nome = "Vinicius";
+        String nomeDoMeuCachorro = "Pirata";
+
+
+        //Chamando atribuição da variável
+        idade = 666;
+
+        System.out.println("Idade = " + idade);
+        System.out.println("Nome = " + nome);
+        System.out.println("Nome do meu cachorro = " + nomeDoMeuCachorro);
+    }
+}
