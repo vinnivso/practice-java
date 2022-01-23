898aa3429e775f1f54e5f3e4ae9fef8e58f232d4
vinnivso <vinnivsob@gmail.com>
2022-01-23 11:58:58 -0300

    Feat: ex.4

--- 1-programacao-basica/1-introducao-a-variaveis/src/com/vinnivso/cursojava/introducao/Exercicios.java
+++ 1-programacao-basica/1-introducao-a-variaveis/src/com/vinnivso/cursojava/introducao/Exercicios.java
@@ -12,7 +12,7 @@ public class Exercicios {
 
         /*
          * 2. Faça um programa que peça um número e então mostre a mensagem "O número informado foi x".
-         */
+        */
         System.out.println("Digite um número inteiro");
         Scanner scan = new Scanner(System.in);
         int numeroInformado = scan.nextInt();
@@ -21,11 +21,26 @@ public class Exercicios {
 
         /*
          * 3. Faça um programa que peça dois números e imprima a soma.
-         */
-        System.out.println("Digite o valor do primeiro número");
+        */
+        System.out.println("Digite o valor do primeiro número inteiro");
         int primeiroNumero = scan.nextInt();
-        System.out.println("Digite o valor do segundo número");
+        System.out.println("Digite o valor do segundo número inteiro");
         int segundoNumero = scan.nextInt();
         System.out.println("O valor da soma é: " + (primeiroNumero + segundoNumero));
+
+
+        /*
+         * 4. Faça um programa que peça as 4 notas bimetras e mostre a média.
+        */
+        System.out.println("Informe a nota da P1, seguindo a notação de ponto flutuante");
+        double  notaP1 = scan.nextDouble();
+        System.out.println("Informe a nota da P2, seguindo a notação de ponto flutuante");
+        double  notaP2 = scan.nextDouble();
+        System.out.println("Informe a nota da P3, seguindo a notação de ponto flutuante");
+        double  notaP3 = scan.nextDouble();
+        System.out.println("Informe a nota da P4, seguindo a notação de ponto flutuante");
+        double  notaP4 = scan.nextDouble();
+        double media = (notaP1 + notaP2 + notaP3 + notaP4) / 4;
+        System.out.println("A média é: " + media);
     }
 }
