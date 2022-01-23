bdc274d589e54cd92b7f349915d38be9d0797e5d
vinnivso <vinnivsob@gmail.com>
2022-01-23 11:46:29 -0300

    Feat: ex.3

--- 1-programacao-basica/1-introducao-a-variaveis/src/com/vinnivso/cursojava/introducao/Exercicios.java
+++ 1-programacao-basica/1-introducao-a-variaveis/src/com/vinnivso/cursojava/introducao/Exercicios.java
@@ -17,5 +17,15 @@ public class Exercicios {
         Scanner scan = new Scanner(System.in);
         int numeroInformado = scan.nextInt();
         System.out.println("O número informado foi " + numeroInformado);
+
+
+        /*
+         * 3. Faça um programa que peça dois números e imprima a soma.
+         */
+        System.out.println("Digite o valor do primeiro número");
+        int primeiroNumero = scan.nextInt();
+        System.out.println("Digite o valor do segundo número");
+        int segundoNumero = scan.nextInt();
+        System.out.println("O valor da soma é: " + (primeiroNumero + segundoNumero));
     }
 }
