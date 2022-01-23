9912b14a72a635f7ba3e4ad738ca0c543b723459
vinnivso <vinnivsob@gmail.com>
2022-01-23 11:35:42 -0300

    Feat. ex1

---
+++ 1-programacao-basica/1-introducao-a-variaveis/src/com/vinnivso/cursojava/introducao/Exercicios.java
@@ -0,0 +1,10 @@
+package com.vinnivso.cursojava.introducao;
+
+public class Exercicios {
+    public static void main(String[] args) {
+        /*
+        * 1. Faça um programa que mostre "Alô mundo" na tela.
+        */
+        System.out.println("Alô mundo");
+    }
+}
