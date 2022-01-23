e0acd23ed6276591af681849c279ad1b867ba850
vinnivso <vinnivsob@gmail.com>
2022-01-23 15:35:07 -0300

    Feat: ex.12

--- 1-programacao-basica/1-introducao-a-variaveis/src/com/vinnivso/cursojava/introducao/Exercicios.java
+++ 1-programacao-basica/1-introducao-a-variaveis/src/com/vinnivso/cursojava/introducao/Exercicios.java
@@ -103,5 +103,26 @@ public class Exercicios {
         double grausCelsiusConverter = scan.nextDouble();
         double grausFahrenheitConverter = (grausCelsiusConverter * 5 / 9) + 32;
         System.out.println("Temperatura em Celsius é: " + decimalFormat.format(grausFahrenheitConverter)+"ºC");
+
+
+        /*
+         * 12. Peça 2 números inteiros e 1 real. Realize operações:
+         * A. O produto do dobro do primeiro com metade do segundo.
+         * B. A soma do triplo do primeiro com o terceiro.
+         * C. O terceiro elevado ao cubo.
+        */
+        System.out.println("Entre com um número inteiro.");
+        long numero1 = scan.nextInt();
+        System.out.println("Entre com um segundo número inteiro.");
+        long numero2 = scan.nextInt();
+        System.out.println("Entre com um número real.");
+        double numero3 = scan.nextDouble();
+
+        double resultA = (2 * numero1) * (numero2 / 2);
+        System.out.println("Resultado A: " + decimalFormat.format(resultA));
+        double resultB = (3 * numero1) + numero3;
+        System.out.println("Resultado B: " + decimalFormat.format(resultB));
+        double resultC = Math.pow(numero3, 3);
+        System.out.println("Resultado C: " + decimalFormat.format(resultC));
     }
 }
