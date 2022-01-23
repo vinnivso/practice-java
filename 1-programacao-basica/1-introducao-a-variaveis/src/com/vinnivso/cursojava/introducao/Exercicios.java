e493b95d80d5b0c297d6a5e703d00208a89dd332
vinnivso <vinnivsob@gmail.com>
2022-01-23 11:40:23 -0300

    Feat: ex.2

--- 1-programacao-basica/1-introducao-a-variaveis/src/com/vinnivso/cursojava/introducao/Exercicios.java
+++ 1-programacao-basica/1-introducao-a-variaveis/src/com/vinnivso/cursojava/introducao/Exercicios.java
@@ -1,10 +1,21 @@
 package com.vinnivso.cursojava.introducao;
 
+import java.util.Scanner;
+
 public class Exercicios {
     public static void main(String[] args) {
         /*
         * 1. Faça um programa que mostre "Alô mundo" na tela.
         */
         System.out.println("Alô mundo");
+
+
+        /*
+         * 2. Faça um programa que peça um número e então mostre a mensagem "O número informado foi x".
+         */
+        System.out.println("Digite um número inteiro");
+        Scanner scan = new Scanner(System.in);
+        int numeroInformado = scan.nextInt();
+        System.out.println("O número informado foi " + numeroInformado);
     }
 }
