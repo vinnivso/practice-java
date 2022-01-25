c421782f93b8db03558b08c4384cfcb9e8908ae9
vinnivso <vinnivsob@gmail.com>
2022-01-25 13:49:55 -0300

    Feat: ex.5

--- 2-controles-decisoes-loops/src/com/vinnivso/cursojava/ExerciciosCondicionais.java
+++ 2-controles-decisoes-loops/src/com/vinnivso/cursojava/ExerciciosCondicionais.java
@@ -8,13 +8,13 @@ public class ExerciciosCondicionais {
 
 //        //1. Programa que peça dois números e imprima o maior deles.
 //        System.out.println("Digite o primeiro valor");
-//        double primeiroValor = scan.nextDouble();
+//        double primeiroValor1 = scan.nextDouble();
 //        System.out.println("Digite o segundo valor");
 //        double segundoValor = scan.nextDouble();
-//        if (primeiroValor > segundoValor) {
-//            System.out.println(primeiroValor + " é maior que " + segundoValor);
-//        } else if (segundoValor > primeiroValor) {
-//            System.out.println(segundoValor + " é maior que " + primeiroValor);
+//        if (primeiroValor1 > segundoValor) {
+//            System.out.println(primeiroValor1 + " é maior que " + segundoValor);
+//        } else if (segundoValor > primeiroValor1) {
+//            System.out.println(segundoValor + " é maior que " + primeiroValor1);
 //        } else {
 //            System.out.println("Os valores informados são equivalentes");
 //        }
@@ -28,8 +28,8 @@ public class ExerciciosCondicionais {
 //        } else {
 //            System.out.println("O valor informado é positivo: " + valorInformado);
 //        }
-
-
+//
+//
 //        //3. Programa que verifica "M" ou "F"
 //        System.out.println("Informe seu sexo M para Masculino ou F para Feminino");
 //        String stringInformada = scan.nextLine();
@@ -38,15 +38,34 @@ public class ExerciciosCondicionais {
 //            case "F" -> System.out.println(stringInformada + " - Feminino");
 //            default -> System.out.println(stringInformada + " Representa um sexo inválido");
 //        }
+//
+//
+//        //4. Programa que verifique vogal ou consoante.
+//        System.out.println("Digite qualquer letra para verificar se é vogal ou consoante");
+//        String stringVogalConsoante = scan.nextLine();
+//        switch (stringVogalConsoante.toUpperCase()) {
+//            case "A","E","I","O","U" -> System.out.println("Caracter informado corresponde uma " +
+//                    "vogal");
+//            default -> System.out.println("Caracter informado corresponde uma consoante");
+//        }
 
 
-        //4. Programa que verifique vogal ou consoante.
-        System.out.println("Digite qualquer letra para verificar se é vogal ou consoante");
-        String stringVogalConsoante = scan.nextLine();
-        switch (stringVogalConsoante.toUpperCase()) {
-            case "A","E","I","O","U" -> System.out.println("Caracter informado corresponde uma " +
-                    "vogal");
-            default -> System.out.println("Caracter informado corresponde uma consoante");
+        //5. Programa para ler duas notas parciais de um aluno.
+        System.out.println("Informe a primeira nota");
+        double primeiraNota = scan.nextDouble();
+        System.out.println("Informe a segunda nota");
+        double segundaNota = scan.nextDouble();
+        double mediaDoAluno = (primeiraNota + segundaNota) / 2;
+        if (mediaDoAluno >= 7 && mediaDoAluno < 10) {
+            System.out.println("Aprovado");
+        } else if (mediaDoAluno == 10) {
+            System.out.println("Aprovado com distinção");
+        } else if (mediaDoAluno > 10) {
+            System.out.println("A nota máxima possível é 10");
+        } else if (mediaDoAluno < 0) {
+            System.out.println("A nota mínima possível é 0");
+        } else {
+            System.out.println("Reprovado");
         }
     }
 }
