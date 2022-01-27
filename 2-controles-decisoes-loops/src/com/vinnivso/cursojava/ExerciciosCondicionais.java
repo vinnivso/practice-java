5c11482331c1081de013cb7ce78fe9913627e3be
vinnivso <vinnivsob@gmail.com>
2022-01-27 12:42:48 -0300

    Feat: ex.14

--- 2-controles-decisoes-loops/src/com/vinnivso/cursojava/ExerciciosCondicionais.java
+++ 2-controles-decisoes-loops/src/com/vinnivso/cursojava/ExerciciosCondicionais.java
@@ -220,6 +220,27 @@ public class ExerciciosCondicionais {
 //        }
 
 
-        //14.
+        //14. Programa para ler notas e dizer APROVADO/REPROVADO
+        System.out.println("Informe a nota da P1");
+        double notaP1 = scan.nextDouble();
+        System.out.println("Informe a nota da P2");
+        double notaP2 = scan.nextDouble();
+
+        double mediaProvas = (notaP1 + notaP2) / 2;
+        String conceito = null;
+        if (mediaProvas <= 10 && mediaProvas > 9) {
+            conceito = "A";
+        } else if (mediaProvas <= 9 && mediaProvas > 7.5) {
+            conceito = "B";
+        } else if (mediaProvas <= 7.5 && mediaProvas > 6.0) {
+            conceito = "C";
+        } else if (mediaProvas <= 6.0 && mediaProvas > 4.0) {
+            conceito = "D";
+        } else if (mediaProvas <= 4.0 && mediaProvas >= 0) {
+            conceito = "E";
+        } else {
+            conceito = "Inválido";
+        }
+        System.out.println("O conceito do aluno é: " + conceito);
     }
 }
