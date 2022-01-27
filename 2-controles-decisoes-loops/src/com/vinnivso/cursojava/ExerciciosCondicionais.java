eafa485c40c7c3c51aa438537c335cb239cbf824
vinnivso <vinnivsob@gmail.com>
2022-01-27 16:56:41 -0300

    Feat: ex.18

--- 2-controles-decisoes-loops/src/com/vinnivso/cursojava/ExerciciosCondicionais.java
+++ 2-controles-decisoes-loops/src/com/vinnivso/cursojava/ExerciciosCondicionais.java
@@ -296,13 +296,26 @@ public class ExerciciosCondicionais {
 //        }
 
 
-        //17. Ano bissexto ou não?
-        System.out.println("Informe o ano que deseja saber se é bissexto");
-        long anoInformado = input.nextLong();
-        if ((anoInformado % 4 == 0 && anoInformado % 100 != 0) || (anoInformado % 400 == 0)) {
-            System.out.println("O ano inserido é bissexto.");
-        } else {
-            System.out.println("O ano inserido não é bissexto.");
+//        //17. Ano bissexto ou não?
+//        System.out.println("Informe o ano que deseja saber se é bissexto");
+//        long anoInformado = input.nextLong();
+//        if ((anoInformado % 4 == 0 && anoInformado % 100 != 0) || (anoInformado % 400 == 0)) {
+//            System.out.println("O ano inserido é bissexto.");
+//        } else {
+//            System.out.println("O ano inserido não é bissexto.");
+//        }
+
+
+        //18. Par ou ímpar?
+        System.out.println("Informe um número natural");
+        int numeroNatural = input.nextInt();
+        if (numeroNatural == 0) {
+            System.out.println("O número: " + numeroNatural + " não corresponde a classificação ÍMPAR ou PAR");
+        } else if (numeroNatural % 2 == 0) {
+            System.out.println("O número: " + numeroNatural + " é PAR");
+        }
+        else {
+            System.out.println("O número: " + numeroNatural + " é ÍMPAR");
         }
 
     }
