a5746a403e01a73618c361ffc3fcbbfa13a30ea5
vinnivso <vinnivsob@gmail.com>
2022-01-27 15:12:34 -0300

    Feat: ex.15

--- 2-controles-decisoes-loops/src/com/vinnivso/cursojava/ExerciciosCondicionais.java
+++ 2-controles-decisoes-loops/src/com/vinnivso/cursojava/ExerciciosCondicionais.java
@@ -220,27 +220,60 @@ public class ExerciciosCondicionais {
 //        }
 
 
-        //14. Programa para ler notas e dizer APROVADO/REPROVADO
-        System.out.println("Informe a nota da P1");
-        double notaP1 = scan.nextDouble();
-        System.out.println("Informe a nota da P2");
-        double notaP2 = scan.nextDouble();
+//        //14. Programa para ler notas e dizer APROVADO/REPROVADO
+//        System.out.println("Informe a nota da P1");
+//        double notaP1 = scan.nextDouble();
+//        System.out.println("Informe a nota da P2");
+//        double notaP2 = scan.nextDouble();
+//
+//        double mediaProvas = (notaP1 + notaP2) / 2;
+//        String conceito = null;
+//        if (mediaProvas <= 10 && mediaProvas > 9) {
+//            conceito = "A";
+//        } else if (mediaProvas <= 9 && mediaProvas > 7.5) {
+//            conceito = "B";
+//        } else if (mediaProvas <= 7.5 && mediaProvas > 6.0) {
+//            conceito = "C";
+//        } else if (mediaProvas <= 6.0 && mediaProvas > 4.0) {
+//            conceito = "D";
+//        } else if (mediaProvas <= 4.0 && mediaProvas >= 0) {
+//            conceito = "E";
+//        } else {
+//            conceito = "Inválido";
+//        }
+//        System.out.println("Nota da P1: " + notaP1);
+//        System.out.println("Nota da P2: " + notaP2);
+//        System.out.println("Média das Notas: " + decimalFormat.format(mediaProvas));
+//        System.out.println("O conceito do aluno é: " + conceito);
+//
+//        switch (conceito) {
+//            case "A", "B", "C" -> System.out.println("APROVADO");
+//            case "D", "E" -> System.out.println("REPROVADO");
+//            default -> System.out.println("INVÁLIDO");
+//        }
 
-        double mediaProvas = (notaP1 + notaP2) / 2;
-        String conceito = null;
-        if (mediaProvas <= 10 && mediaProvas > 9) {
-            conceito = "A";
-        } else if (mediaProvas <= 9 && mediaProvas > 7.5) {
-            conceito = "B";
-        } else if (mediaProvas <= 7.5 && mediaProvas > 6.0) {
-            conceito = "C";
-        } else if (mediaProvas <= 6.0 && mediaProvas > 4.0) {
-            conceito = "D";
-        } else if (mediaProvas <= 4.0 && mediaProvas >= 0) {
-            conceito = "E";
+
+        //15. Programa verifica se é um triângulo e qual característica do mesmo, escaleno, isósceles ou equilátero.
+        System.out.println("Entre com o valor do primeiro lado");
+        double valorPrimeiroLado = scan.nextDouble();
+        System.out.println("Entre com o valor do segundo lado");
+        double valorSegundoLado = scan.nextDouble();
+        System.out.println("Entre com o valor do terceiro lado");
+        double valorTerceiroLado = scan.nextDouble();
+
+        if (((valorPrimeiroLado + valorSegundoLado) > valorTerceiroLado) && ((valorPrimeiroLado + valorTerceiroLado) > valorSegundoLado) && ((valorSegundoLado + valorTerceiroLado) > valorPrimeiroLado)) {
+            if (valorPrimeiroLado == valorSegundoLado && valorPrimeiroLado == valorTerceiroLado) {
+                System.out.println("Equilátero");
+            } else if (valorPrimeiroLado != valorSegundoLado && valorPrimeiroLado != valorTerceiroLado) {
+                System.out.println("Isósceles");
+            } else {
+                System.out.println("Escaleno");
+            }
         } else {
-            conceito = "Inválido";
+            System.out.println("Os valores informados não correspondem com um triângulo");
         }
-        System.out.println("O conceito do aluno é: " + conceito);
+
+
+        //16.
     }
 }
