a53443cd85338843f27bc0ad2db0d80f3543873e
vinnivso <vinnivsob@gmail.com>
2022-01-28 17:36:56 -0300

    Feat: ex.5

--- 2-controles-decisoes-loops/src/com/vinnivso/cursojava/ExerciciosLoops.java
+++ 2-controles-decisoes-loops/src/com/vinnivso/cursojava/ExerciciosLoops.java
@@ -126,15 +126,82 @@ public class ExerciciosLoops {
 //        System.out.println("Estado civil: " + estadoCivil);
 
 
-        //4. Programa que calcule e escreve quantos anos a população de um país A vai ultrapassar país B.
-        int popA = 80000;
-        int popB = 200000;
+//        //4. Programa que calcule e escreve quantos anos a população de um país A vai ultrapassar país B.
+//        int popA = 80000;
+//        int popB = 200000;
+//        int i = 0;
+//        while (popA <= popB) {
+//            popA += popA * .03;
+//            popB += popB * .015;
+//            i++;
+//        }
+//        System.out.println("População do País A: " + popA);
+//        System.out.println("População do País B: " + popB);
+//        System.out.println("Para o país A ultrapassar ou igualar com a população do país B, vão demorar " + i + " anos");
+
+
+        //5. Programa que refatore o EX.4, usuário passando os parâmetros dos países.
+        boolean validaInputDoUsuario = false;
+        int popAUser;
+        int popBUser;
+        double crescimentoA;
+        double crescimentoB;
         int i = 0;
-        while (popA <= popB) {
-            popA += popA * .03;
-            popB += popB * .015;
+
+        do {
+            System.out.println("Informe a população do país A");
+            popAUser = input.nextInt();
+            if (!(popAUser >= 0)) {
+                System.out.println("O valor da população deve ser maior que ZERO");
+            } else {
+                validaInputDoUsuario = true;
+                System.out.println("Informação da população A: " + popAUser);
+            }
+        } while (!validaInputDoUsuario);
+
+        validaInputDoUsuario = false;
+        do {
+            System.out.println("Informe a população do país B");
+            popBUser = input.nextInt();
+            if (!(popBUser >= 0)) {
+                System.out.println("O valor da população deve ser maior que ZERO");
+            } else {
+                validaInputDoUsuario = true;
+                System.out.println("Informação da população B: " + popBUser);
+            }
+        } while (!validaInputDoUsuario);
+
+        validaInputDoUsuario = false;
+        do {
+            System.out.println("Informe a taxa de crescimento do país A");
+            crescimentoA = input.nextInt();
+            if (!(crescimentoA >= 0)) {
+                System.out.println("O valor da taxa de crescimento deve ser maior que ZERO");
+            } else {
+                validaInputDoUsuario = true;
+                System.out.println("Taxa de crescimento da população A: " + crescimentoA + "%");
+            }
+        } while (!validaInputDoUsuario);
+
+        validaInputDoUsuario = false;
+        do {
+            System.out.println("Informe a taxa de crescimento do país B");
+            crescimentoB = input.nextInt();
+            if (!(crescimentoB >= 0)) {
+                System.out.println("O valor da taxa de crescimento deve ser maior que ZERO");
+            } else {
+                validaInputDoUsuario = true;
+                System.out.println("Taxa de crescimento da população B: " + crescimentoB + "%");
+            }
+        } while (!validaInputDoUsuario);
+
+        while (popAUser <= popBUser) {
+            popAUser += popAUser * (crescimentoA / 100);
+            popBUser += popBUser * (crescimentoB / 100);
             i++;
         }
+        System.out.println("População do País A: " + popAUser);
+        System.out.println("População do País B: " + popBUser);
         System.out.println("Para o país A ultrapassar ou igualar com a população do país B, vão demorar " + i + " anos");
     }
 }
