5dfe1b5e071fdf3305078cdacea944464a5cf994
vinnivso <vinnivsob@gmail.com>
2022-01-28 16:01:07 -0300

    Feat: ex.2

--- 2-controles-decisoes-loops/src/com/vinnivso/cursojava/ExerciciosLoops.java
+++ 2-controles-decisoes-loops/src/com/vinnivso/cursojava/ExerciciosLoops.java
@@ -11,17 +11,41 @@ public class ExerciciosLoops {
         //1. Valide número inserido por usuário.
         System.out.println("insira a nota");
         boolean notaValida = false;
+        double nota; // -> Por uma questão de performance, será declarado as variáveis que serão utilizadas
+        // no loop, fora do escopo do loop.
 
         do {
             System.out.println("Entre com uma nota");
-            double nota = input.nextDouble();
+            nota = input.nextDouble();
             if (nota >= 0 && nota <= 10) {
                 notaValida = true;
                 System.out.println("Você digitou: " + nota);
             } else {
+                //notaValida = true; -> Sem necessidade uma vez que a outra condição já está como true.
                 System.out.println("Nota inválida, digite novamente");
             }
         } while (!notaValida);
+
+
+        //2. Programa que leia nome de usuário e senha, não aceita senha igual ao nome de usuário, mostrando erro e
+        // pedindo para inserir novamente.
+        boolean userNameValido = false;
+        String userNameInput;
+        String passwordInput;
+
+        do {
+            System.out.println("Entre com o nome de usuário: ");
+            userNameInput = input.next();
+            System.out.println("Entre com a senha: ");
+            passwordInput = input.next();
+            if (userNameInput.equalsIgnoreCase(passwordInput)) {
+                //userNameValido = false; -> Sem necessidade uma vez que a outra condição já está como true.
+                System.out.println("Senha igual a usuário, digite novamente");
+            } else {
+                userNameValido = true;
+                System.out.println("Senha e usuário informado estão válidos.");
+            }
+        } while (!userNameValido);
 
     }
 }
