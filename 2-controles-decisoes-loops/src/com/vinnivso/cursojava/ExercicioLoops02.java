adaff76975043a9b8e59fae728045a4b5bf07973
vinnivso <vinnivsob@gmail.com>
2022-01-29 17:10:05 -0300

    Feat: ExercicioLoops02

---
+++ 2-controles-decisoes-loops/src/com/vinnivso/cursojava/ExercicioLoops02.java
@@ -0,0 +1,31 @@
+package com.vinnivso.cursojava;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class ExercicioLoops02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+        //2. Programa que leia nome de usuário e senha, não aceita senha igual ao nome de usuário, mostrando erro e
+        // pedindo para inserir novamente.
+        boolean userNameValido = false;
+        String userNameInput;
+        String passwordInput;
+
+        do {
+            System.out.println("Entre com o nome de usuário: ");
+            userNameInput = input.next();
+            System.out.println("Entre com a senha: ");
+            passwordInput = input.next();
+            if (userNameInput.equalsIgnoreCase(passwordInput)) {
+                //userNameValido = false; -> Sem necessidade uma vez que a outra condição já está como true.
+                System.out.println("Senha igual a usuário, digite novamente");
+            } else {
+                userNameValido = true;
+                System.out.println("Senha e usuário informado estão válidos.");
+            }
+        } while (!userNameValido);
+    }
+}
