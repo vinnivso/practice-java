6a7b206b1887b0e2ad5f5550a00f83333a30fe60
vinnivso <vinnivsob@gmail.com>
2022-01-29 19:16:22 -0300

    Feat: ExercicioLoops36

---
+++ 2-controles-decisoes-loops/src/com/vinnivso/cursojava/exercicioloops/ExercicioLoops36.java
@@ -0,0 +1,20 @@
+package com.vinnivso.cursojava.exercicioloops;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class ExercicioLoops36 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+        System.out.println("Digite o valor de n:");
+        int n = input.nextInt();
+        double soma = 0;
+
+        for (int i = 1; i <= n; i++) {
+            soma += 1 / i;
+        }
+        System.out.println("Soma = " + decimalFormat.format(soma));
+    }
+}
