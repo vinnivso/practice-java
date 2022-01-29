68f95da62e46a1da311321248b1250f033158f1e
vinnivso <vinnivsob@gmail.com>
2022-01-29 18:50:42 -0300

    Feat: ExercicioLoops24

--- 2-controles-decisoes-loops/src/com/vinnivso/cursojava/exercicioloops/ExercicioLoops24.java
+++ 2-controles-decisoes-loops/src/com/vinnivso/cursojava/exercicioloops/ExercicioLoops24.java
@@ -3,14 +3,15 @@ package com.vinnivso.cursojava.exercicioloops;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class ExercicioLoops23 {
+public class ExercicioLoops24 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         DecimalFormat decimalFormat = new DecimalFormat("0.00");
 
-        System.out.println("Lojas Quase Dois - Tabela de preços");
+        System.out.println("Preço do pão: R$ 0.18");
+        System.out.println("Panificadora Pão de Ontem - Tabela de preços");
         for (int i=1; i<=50; i++){
-            System.out.println(i + " - R$ " + decimalFormat.format((1.99*i)));
+            System.out.println(i + " - R$ " + decimalFormat.format((0.18*i)));
         }
     }
 }
