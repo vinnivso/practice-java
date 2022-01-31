36a6b71958977197ca47cc6bbe04e0bb6b5516de
vinnivso <vinnivsob@gmail.com>
2022-01-31 12:45:19 -0300

    Feat: ExercicioVetores04

---
+++ 3-vetores-matrizes/src/com/vinnivso/cursojava/exerciciovetores/ExercicioVetores04.java
@@ -0,0 +1,35 @@
+package com.vinnivso.cursojava.exerciciovetores;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class ExercicioVetores04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+        int[] arrayA = new int[15];
+        arrayA[0] = 1;
+        arrayA[1] = 2;
+        arrayA[2] = 3;
+        arrayA[3] = 4;
+        arrayA[4] = 5;
+        arrayA[5] = 6;
+        arrayA[6] = 7;
+        arrayA[7] = 8;
+        arrayA[8] = 9;
+        arrayA[9] = 10;
+        arrayA[10] = 11;
+        arrayA[11] = 12;
+        arrayA[12] = 13;
+        arrayA[13] = 14;
+        arrayA[14] = 15;
+        int arrayB[] = arrayA;
+
+        for (double element : arrayB) {
+            element = Math.sqrt(element);
+            System.out.println("O resultado das ráizes quadradas dos valores presentes no Array A, são: " + decimalFormat.format(element));
+        }
+
+    }
+}
