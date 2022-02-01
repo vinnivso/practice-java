151b98f84df633c67c6fbb30816fbc9a5dcac689
vinnivso <vinnivsob@gmail.com>
2022-02-01 12:41:53 -0300

    Feat: ExercicioVetores09

---
+++ 3-vetores-matrizes/src/com/vinnivso/cursojava/exerciciovetores/ExercicioVetores09.java
@@ -0,0 +1,44 @@
+package com.vinnivso.cursojava.exerciciovetores;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class ExercicioVetores09 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[vetorA.length];
+        double[] vetorC = new double[vetorA.length];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.println("Entre com o valor do vetor A, na posição: " + i);
+            vetorA[i] = input.nextInt();
+        }
+        for (int i = 0; i < vetorB.length; i++) {
+            System.out.println("Entre com o valor do vetor B, na posição: " + i);
+            vetorB[i] = input.nextInt();
+        }
+        for (int i = 0; i < vetorC.length; i++) {
+            vetorC[i] = (float) vetorA[i] / vetorB[i];
+        }
+
+        System.out.print("Vetor A = ");
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.print(decimalFormat.format(vetorA[i]) + " ");
+        }
+        System.out.println();
+
+        System.out.print("Vetor B = ");
+        for (int i = 0; i < vetorB.length; i++) {
+            System.out.print(decimalFormat.format(vetorB[i]) + " ");
+        }
+        System.out.println();
+
+        System.out.print("Vetor C = ");
+        for (int i = 0; i < vetorC.length; i++) {
+            System.out.print(decimalFormat.format(vetorC[i]) + " ");
+        }
+    }
+}
