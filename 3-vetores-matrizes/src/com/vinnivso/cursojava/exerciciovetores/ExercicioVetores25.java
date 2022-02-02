3e5966aa6cba313de56151a98d43745f792b9bae
vinnivso <vinnivsob@gmail.com>
2022-02-01 23:46:17 -0300

    Feat: ExercicioVetores25

--- 3-vetores-matrizes/src/com/vinnivso/cursojava/exerciciovetores/ExercicioVetores25.java
+++ 3-vetores-matrizes/src/com/vinnivso/cursojava/exerciciovetores/ExercicioVetores25.java
@@ -3,34 +3,29 @@ package com.vinnivso.cursojava.exerciciovetores;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class ExercicioVetores24 {
+public class ExercicioVetores25 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         DecimalFormat decimalFormat = new DecimalFormat("0.00");
 
         int[] vetorA = new int[10];
+        int[] vetorB = new int[vetorA.length];
 
         for (int i = 0; i < vetorA.length; i++) {
             System.out.println("Entre com um número para a posição " + i);
             vetorA[i] = input.nextInt();
+            vetorB[i] = (vetorA[i] % 2 == 0) ? 1 : 0;
         }
-        //1221
-        //i = 2
-        boolean palindromo = true;
-        for (int i = 0; i < (vetorA.length / 2); i++) {
-
-            if (vetorA[i] != vetorA[vetorA.length - 1 - i]) {
-                palindromo = false;
-                break;
-            }
-        }
-
         System.out.print("Vetor A = ");
         for (int i = 0; i < vetorA.length; i++) {
             System.out.print(vetorA[i] + " ");
         }
         System.out.println();
-        if (palindromo) System.out.println("Palindromo");
-        else System.out.println("Não é palindromo");
+
+        System.out.print("Vetor B = ");
+        for (int i = 0; i < vetorB.length; i++) {
+            System.out.print(vetorB[i] + " ");
+        }
+        System.out.println();
     }
 }
