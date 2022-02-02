e7a1ab6d16df974dc27e5143cd272e79eb748bbe
vinnivso <vinnivsob@gmail.com>
2022-02-02 00:10:22 -0300

    Feat: ExercicioVetores34

--- 3-vetores-matrizes/src/com/vinnivso/cursojava/exerciciovetores/ExercicioVetores34.java
+++ 3-vetores-matrizes/src/com/vinnivso/cursojava/exerciciovetores/ExercicioVetores34.java
@@ -3,33 +3,26 @@ package com.vinnivso.cursojava.exerciciovetores;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class ExercicioVetores33 {
+public class ExercicioVetores34 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         DecimalFormat decimalFormat = new DecimalFormat("0.00");
 
-        int[] vetorA = new int[5];
+        int[] vetorA = new int[10];
 
         for (int i = 0; i < vetorA.length; i++) {
             System.out.println("Entre com o valor da posição A - " + i);
             vetorA[i] = input.nextInt();
         }
 
-        boolean primo;
-        String msg;
-
         for (int i = 0; i < vetorA.length; i++) {
-            primo = true;
+            System.out.println("Analizando o número " + vetorA[i]);
             for (int j = 2; j < vetorA[i]; j++) {
-                if (vetorA[i] % j == 0) {
-                    primo = false;
-                    break;
+                if (j % 2 == 0) {
+                    System.out.println(j + " é par");
                 }
             }
-            msg = "";
-            if (primo) msg = " primo";
-            else msg = " não é primo";
-            System.out.println(vetorA[i] + msg);
+            System.out.println();
         }
     }
 }
