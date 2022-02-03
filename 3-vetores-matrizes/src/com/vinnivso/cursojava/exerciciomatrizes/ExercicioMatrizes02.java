9eb5d687c81cefacfac213f3e4489ee42f11a05c
vinnivso <vinnivsob@gmail.com>
2022-02-03 04:26:01 -0300

    Feeat: ExercicioMatrizes03

--- 3-vetores-matrizes/src/com/vinnivso/cursojava/exerciciomatrizes/ExercicioMatrizes02.java
+++ 3-vetores-matrizes/src/com/vinnivso/cursojava/exerciciomatrizes/ExercicioMatrizes02.java
@@ -18,8 +18,8 @@ public class ExercicioMatrizes02 {
             }
         }
 
-        for (int i=0; i<numerosAleatorios.length; i++){
-            for (int j=0; j<numerosAleatorios[i].length; j++){
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            for (int j = 0; j < numerosAleatorios[i].length; j++) {
                 System.out.print(numerosAleatorios[i][j] + " ");
             }
             System.out.println();
@@ -28,7 +28,7 @@ public class ExercicioMatrizes02 {
         int maiorL5 = 0;
         int menorL5 = 101;
         int linha5 = 5;
-        for (int i=0; i<numerosAleatorios[linha5].length; i++){
+        for (int i = 0; i < numerosAleatorios[linha5].length; i++) {
             if (numerosAleatorios[linha5][i] > maiorL5) maiorL5 = numerosAleatorios[linha5][i];
             if (numerosAleatorios[linha5][i] < menorL5) menorL5 = numerosAleatorios[linha5][i];
         }
@@ -38,7 +38,7 @@ public class ExercicioMatrizes02 {
         int maiorC7 = 0;
         int menorC7 = 101;
         int col7 = 7;
-        for (int i=0; i<numerosAleatorios.length; i++){
+        for (int i = 0; i < numerosAleatorios.length; i++) {
             if (numerosAleatorios[i][col7] > maiorC7) maiorC7 = numerosAleatorios[i][col7];
             if (numerosAleatorios[i][col7] < menorC7) menorC7 = numerosAleatorios[i][col7];
         }
