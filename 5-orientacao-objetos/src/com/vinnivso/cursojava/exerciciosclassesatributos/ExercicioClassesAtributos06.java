b2cada068313da1db7470ac586bcdb2b531f545e
vinnivso <vinnivsob@gmail.com>
2022-02-06 16:46:58 -0300

    Feat: ExercicioClassesAtributos06

---
+++ 5-orientacao-objetos/src/com/vinnivso/cursojava/exerciciosclassesatributos/ExercicioClassesAtributos06.java
@@ -0,0 +1,21 @@
+package com.vinnivso.cursojava.exerciciosclassesatributos;
+
+public class ExercicioClassesAtributos06 {
+    static class Contato {
+        String nome;
+        String email;
+        String endereco;
+        String[] telefones;
+    }
+
+    public static void main(String[] args) {
+        Contato contato1 = new Contato();
+        contato1.nome = "João";
+        contato1.endereco = "Av Paulista, 1000";
+        contato1.email = "joao@email.com";
+        contato1.telefones = new String[5];
+        contato1.telefones[0] = "99999-9999";
+        contato1.telefones[1] = "99999-9998";
+        contato1.telefones[2] = "99999-9997";
+    }
+}
